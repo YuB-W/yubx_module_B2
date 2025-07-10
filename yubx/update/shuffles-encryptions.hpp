37c6c0261cff9e52a0ba2fbcc4ef1759fdaea628
YuB-X <128683039+YuB-W@users.noreply.github.com>
2025-07-10 13:01:40 +0300

    Update shuffles-encryptions.hpp

--- yubx/update/shuffles-encryptions.hpp
+++ yubx/update/shuffles-encryptions.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Windows.h>
+#include "Updating/Offsets.hpp"
 
 #define REBASE(x) x + (uintptr_t)GetModuleHandle(nullptr)
 
@@ -148,38 +149,37 @@ public:
 #define LUAU_COMMA_SEP ,
 #define LUAU_SEMICOLON_SEP ;
 
-// ===== VM Shuffle Macros =====
-#define LUAU_SHUFFLE3(s, a1, a2, a3) a1 s a2 s a3
-#define LUAU_SHUFFLE4(s, a1, a2, a3, a4) a1 s a3 s a2 s a4
-#define LUAU_SHUFFLE5(s, a1, a2, a3, a4, a5) a5 s a3 s a1 s a4 s a2
-#define LUAU_SHUFFLE6(s, a1, a2, a3, a4, a5, a6) a2 s a4 s a3 s a1 s a5 s a6
-#define LUAU_SHUFFLE7(s, a1, a2, a3, a4, a5, a6, a7) a2 s a3 s a4 s a1 s a5 s a6 s a7
-#define LUAU_SHUFFLE8(s, a1, a2, a3, a4, a5, a6, a7, a8) a1 s a7 s a3 s a8 s a5 s a6 s a4 s a2
-#define LUAU_SHUFFLE9(s, a1, a2, a3, a4, a5, a6, a7, a8, a9) a7 s a4 s a6 s a9 s a1 s a8 s a2 s a5 s a3
+#define LUAU_SHUFFLE3(s, a1, a2, a3) a3 s a1 s a2
+#define LUAU_SHUFFLE4(s, a1, a2, a3, a4) a1 s a4 s a2 s a3
+#define LUAU_SHUFFLE5(s, a1, a2, a3, a4, a5) a4 s a3 s a2 s a5 s a1
+#define LUAU_SHUFFLE6(s, a1, a2, a3, a4, a5, a6) a5 s a1 s a6 s a3 s a4 s a2
+#define LUAU_SHUFFLE7(s, a1, a2, a3, a4, a5, a6, a7) a2 s a4 s a5 s a7 s a6 s a3 s a1
+#define LUAU_SHUFFLE8(s, a1, a2, a3, a4, a5, a6, a7, a8) a4 s a3 s a2 s a7 s a6 s a5 s a8 s a1
+#define LUAU_SHUFFLE9(s, a1, a2, a3, a4, a5, a6, a7, a8, a9) a1 s a5 s a3 s a2 s a4 s a9 s a8 s a7 s a6
 
-// -- vmval0
-#define PROTO_MEMBER1_ENC        vmval0
-#define LSTATE_GLOBAL_ENC        vmval0
-#define CLOSURE_FUNC_ENC         vmval0
-#define TABLE_MEMBER_ENC         vmval0
-#define TABLE_META_ENC           vmval0
-#define TSTRING_LEN_ENC          vmval0
-#define GSTATE_TTNAME_ENC        vmval0
-#define GSTATE_TMNAME_ENC        vmval0
 
-// -- vmval1
-#define PROTO_DEBUGISN_ENC       vmval1
-#define CLOSURE_CONT_ENC         vmval1
-#define UDATA_META_ENC           vmval1
+#define UDATA_META_ENC        vmval1
+#define CLOSURE_CONT_ENC      vmval1
+#define PROTO_DEBUGISN_ENC    vmval1
 
-// -- vmval2
-#define PROTO_DEBUGNAME_ENC      vmval2
-#define TSTRING_HASH_ENC         vmval2
+#define PROTO_DEBUGNAME_ENC   vmval2
+#define TSTRING_HASH_ENC      vmval2
 
-// -- vmval3
-#define PROTO_MEMBER2_ENC        vmval3
-#define CLOSURE_DEBUGNAME_ENC    vmval3
 
-// -- vmval4
-#define PROTO_TYPEINFO_ENC       vmval4
-#define LSTATE_STACKSIZE_ENC     vmval4
+#define CLOSURE_DEBUGNAME_ENC vmval3
+#define PROTO_MEMBER2_ENC     vmval3
+#define PROTO_TYPEINFO_ENC    vmval4
+#define LSTATE_STACKSIZE_ENC  vmval4
+
+
+
+
+#define TSTRING_LEN_ENC   vmval0
+#define GSTATE_TTNAME_ENC vmval0
+#define GSTATE_TMNAME_ENC vmval0
+#define PROTO_MEMBER1_ENC vmval0
+#define CLOSURE_FUNC_ENC  vmval0
+#define LSTATE_GLOBAL_ENC vmval0
+
+#define TABLE_MEMBER_ENC  vmval0
+#define TABLE_META_ENC    vmval0
